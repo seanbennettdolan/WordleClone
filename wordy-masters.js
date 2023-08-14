0a425c77a325ee7f63ffc735c621660016617523
Sean <seanbennettdolan@gmail.com>
2023-08-14 17:09:57 -0400

    grabs word of the day and compares inputed words to word of the day - if letter is correct it highlights green

--- wordy-masters.js
+++ wordy-masters.js
@@ -5,6 +5,14 @@ const ANSWER_LENGTH = 5;
 async function init() {
     let currentGuess = '';
     let currentRow = 0;
+
+    const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const resObj = await res.json();
+    const word = resObj.word.toUpperCase();
+    const wordParts = word.split("");
+    setLoading(false);
+
+    console.log(word);
  
     function addLetter (letter) {
         if (currentGuess.length < ANSWER_LENGTH) {
@@ -23,14 +31,34 @@ async function init() {
             // do nothing
             return;
         }
+
+        // TODO validate the word
+
+        // TODO do all the marking as 'correct' 'close' or 'wrong' 
+
+        const guessParts = currentGuess.split("");
+
+        for (let i = 0; i < ANSWER_LENGTH; i++) {
+            // mark as correct
+            if (guessParts[i] === wordParts[i]) {
+letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+            }
+        }
+
+        // TODO did they win or lose?
+
         currentRow++;
         currentGuess = "";
+    }
+
+    function backspace() {
+        currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
     }
 
     document.addEventListener('keydown', function handleKeyPress (event){
 const action = event.key;
 
-console.log(action);
 
 if (action === 'Enter') {
     commit();
@@ -46,6 +74,10 @@ backspace();
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
+  }
+
+  function setLoading(isLoading) {
+loadingDiv.classList.toggle('hidden', !isLoading);
   }
 
 init();
